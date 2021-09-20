305b2ee0fa53f7a972e1f05c49b77d09d07a050f
Sriram Nair <shnair1995@gmail.com>
2021-09-20 15:15:34 -0700

    Demo Completed

---
+++ index.js
@@ -0,0 +1,21 @@
+const name = 'Sriram'
+
+let age = 25
+
+const isCool = true 
+
+let car = null;
+
+let height;
+
+console.log(height)
+
+let x = 4 
+let y = 10
+let z = 7 + 8
+let sum = x + y;
+let str = 'This is a string '
+let strTwo = "This is a second string "
+let strThree = `This is my name: ${name}, and this is my age: ${age} `
+let strings = str + strTwo + strThree
+console.log(strings)
